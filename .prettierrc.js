50d088e5ca452e2f3a759dab68fb20f88b7f13f2
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-08-28 01:53:23 -0300

    prettier: delete overrides
    Keep it simple.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,13 +2,4 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-  overrides: [
-    {
-      files: '*.js',
-      options: {
-        parser: 'babel',
-        trailingComma: 'es5',
-      },
-    },
-  ],
 };

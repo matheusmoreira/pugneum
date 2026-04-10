0a1c21a75490945ea3fd863521b78555e574981e
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-10 16:38:25 +0000

    filterer tests: remove dead test code
    Remove two forEach loops that iterated over .input.json files in the
    test/cases/ and test/errors/ directories. No .input.json files exist in
    cases/ and the errors/ directory does not exist at all, so these loops
    silently produced zero tests while appearing to provide coverage.
    
    Also remove the process.chdir() call that only existed to support those
    loops. With node:test all test files run in the same process, making
    global cwd mutation a cross-test contamination risk.
    
    Remove the now-unused fs import.

--- packages/filterer/test/index.test.js
+++ packages/filterer/test/index.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var fs = require('fs');
 var path = require('path');
 var assert = require('node:assert/strict');
 var {test} = require('node:test');
@@ -52,48 +51,4 @@ p
 
   const output = filter(ast, customFilters);
   t.assert.snapshot(output);
-});
-process.chdir(__dirname + '/../');
-
-var testCases;
-
-testCases = fs.readdirSync(__dirname + '/cases').filter(function(name) {
-  return /\.input\.json$/.test(name);
-});
-
-testCases.forEach(function(filename) {
-  function read(path) {
-    return fs.readFileSync(__dirname + '/cases/' + path, 'utf8');
-  }
-
-  test('cases/' + filename, function(t) {
-    var actualAst = JSON.stringify(
-      filter(JSON.parse(read(filename)), customFilters),
-      null,
-      '  '
-    );
-    t.assert.snapshot(actualAst);
-  });
-});
-
-testCases.forEach(function(filename) {
-  function read(path) {
-    return fs.readFileSync(__dirname + '/errors/' + path, 'utf8');
-  }
-
-  test('errors/' + filename, function(t) {
-    var actual;
-    try {
-      filter(JSON.parse(read(filename)), customFilters);
-      throw new Error('Expected ' + filename + ' to throw an exception.');
-    } catch (ex) {
-      if (!ex || !ex.code || ex.code.indexOf('PUGNEUM:') !== 0) throw ex;
-      actual = {
-        msg: ex.msg,
-        code: ex.code,
-        line: ex.line,
-      };
-    }
-    t.assert.snapshot(actual);
-  });
 });

91fb1b4de33623547b211631a34ba4c543fc7401
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-10 17:38:58 +0000

    linker tests: cover error handling paths
    Add tests for linker error conditions:
    - Non-Block top level element throws
    - UNDEFINED_REFERENCE for unknown @[ref] names
    - MISSING_YIELD when include passes a block into a template
      that has no explicit yield block
    
    Also extract a linkFile helper to reduce duplication in the
    existing snapshot test loop.

--- packages/linker/test/index.test.js
+++ packages/linker/test/index.test.js
@@ -1,3 +1,4 @@
+var assert = require('node:assert/strict');
 var fs = require('fs');
 var {describe, test} = require('node:test');
 var lex = require('pugneum-lexer');
@@ -7,23 +8,60 @@ var link = require('../');
 
 var basedir = __dirname + '/cases';
 
+function linkFile(filename) {
+  let source = fs.readFileSync(filename, 'utf8');
+  let options = {filename, source, lex, parse, basedir};
+  let tokens = lex(source, options);
+  let ast = parse(tokens, options);
+  let loaded = load(ast, options);
+  return link(loaded);
+}
+
 function testDir(dir) {
   fs.readdirSync(dir).forEach(function(name) {
     if (!/\.pg$/.test(name)) return;
     test(name, function(t) {
-      let filename = dir + '/' + name;
-      let source = fs.readFileSync(filename, 'utf8');
-      let options = {filename, source, lex, parse, basedir};
-      let tokens = lex(source, options);
-      let ast = parse(tokens, options);
-      let loaded = load(ast, options);
-      var actual = link(loaded);
-
-      t.assert.snapshot(actual);
+      t.assert.snapshot(linkFile(dir + '/' + name));
     });
   });
 }
 
 describe('cases from pugneum sources', function() {
   testDir(__dirname + '/cases');
+});
+
+describe('error handling', () => {
+  test('top level must be a Block', () => {
+    assert.throws(
+      () => link({type: 'Tag', name: 'div'}),
+      /top level.*block/i
+    );
+  });
+
+  test('UNDEFINED_REFERENCE for unknown @[ref]', () => {
+    var filename = basedir + '/auxiliary/layout-append.pg';
+    // Build a minimal AST with an undefined reference
+    var source = 'p @[missing]';
+    var options = {filename: 'test.pg', source, lex, parse, basedir};
+    var tokens = lex(source, options);
+    var ast = parse(tokens, options);
+    var loaded = load(ast, options);
+    assert.throws(
+      () => link(loaded),
+      (err) => err.code === 'PUGNEUM:UNDEFINED_REFERENCE'
+    );
+  });
+
+  test('MISSING_YIELD when include passes block but template has no yield', () => {
+    var dir = __dirname + '/cases';
+    var includer = 'include auxiliary/pet.pg\n  p Extra content';
+    var options = {filename: dir + '/test.pg', source: includer, lex, parse, basedir: dir};
+    var tokens = lex(includer, options);
+    var ast = parse(tokens, options);
+    var loaded = load(ast, options);
+    assert.throws(
+      () => link(loaded),
+      (err) => err.code === 'PUGNEUM:MISSING_YIELD'
+    );
+  });
 });

cfa17a3012b28b2df0f7b915b7711138e66ce791
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-10 17:38:51 +0000

    loader tests: cover error paths and custom options
    Add tests for previously uncovered loader functionality:
    
    Option validation:
    - Non-object options throws TypeError
    - Non-function lex option throws TypeError
    - Non-function parse option throws TypeError
    
    Path resolution errors:
    - Relative path without filename option
    - Absolute path without basedir option
    - Missing file throws ENOENT
    
    Custom options:
    - Custom resolve function is called
    - Custom read function is called and its return value used

--- packages/loader/test/index.test.js
+++ packages/loader/test/index.test.js
@@ -1,8 +1,9 @@
 'use strict';
 
+var assert = require('node:assert/strict');
 var fs = require('fs');
 var path = require('path');
-var {test} = require('node:test');
+var {test, describe} = require('node:test');
 
 var walk = require('pugneum-walker');
 var lex = require('pugneum-lexer');
@@ -29,4 +30,85 @@ test('pugneum-loader', (t) => {
   );
 
   t.assert.snapshot(ast);
+});
+
+describe('option validation', () => {
+  test('throws if options is not an object', () => {
+    assert.throws(
+      () => load({type: 'Block', nodes: []}, 'bad'),
+      /options must be an object/
+    );
+  });
+
+  test('throws if lex is not a function', () => {
+    assert.throws(
+      () => load({type: 'Block', nodes: []}, {lex: 'bad', parse}),
+      /lex.*function/
+    );
+  });
+
+  test('throws if parse is not a function', () => {
+    assert.throws(
+      () => load({type: 'Block', nodes: []}, {lex, parse: 42}),
+      /parse.*function/
+    );
+  });
+});
+
+describe('path resolution', () => {
+  test('throws for relative path without filename', () => {
+    var ast = parse(lex('include foo.pg'), {});
+    assert.throws(
+      () => load(ast, {lex, parse}),
+      /filename.*required/
+    );
+  });
+
+  test('throws for absolute path without basedir', () => {
+    var ast = parse(lex('include /foo.pg', {filename: 'test.pg'}), {filename: 'test.pg'});
+    assert.throws(
+      () => load(ast, {lex, parse}),
+      /basedir.*required/
+    );
+  });
+
+  test('throws for missing file', () => {
+    var filename = __dirname + '/test.pg';
+    var ast = parse(lex('include nonexistent.pg', {filename}), {filename});
+    assert.throws(
+      () => load(ast, {lex, parse}),
+      /ENOENT/
+    );
+  });
+});
+
+describe('custom resolve and read', () => {
+  test('accepts custom resolve function', () => {
+    var filename = __dirname + '/test.pg';
+    var ast = parse(lex('include foo.pg', {filename}), {filename});
+    var customResolveCalled = false;
+    var customResolve = function(file, source, opts) {
+      customResolveCalled = true;
+      return path.join(path.dirname(source), file);
+    };
+    // Will fail because foo.pg doesn't exist, but resolve should be called
+    try {
+      load(ast, {lex, parse, resolve: customResolve});
+    } catch (e) {
+      // Expected: file not found
+    }
+    assert.ok(customResolveCalled, 'custom resolve was called');
+  });
+
+  test('accepts custom read function', () => {
+    var filename = __dirname + '/test.pg';
+    var ast = parse(lex('include other.pg', {filename}), {filename});
+    var readCalled = false;
+    var customRead = function(file) {
+      readCalled = true;
+      return Buffer.from('p hello');
+    };
+    load(ast, {lex, parse, read: customRead});
+    assert.ok(readCalled, 'custom read was called');
+  });
 });

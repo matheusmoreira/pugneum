740fe1e3db925633d7e3289ad35713841027e646
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-12 20:08:29 +0000

    tests: add parser error path coverage
    Four parser error codes had no test coverage. Add explicit tests
    for each:
    
    - BLOCK_OUTSIDE_MIXIN: anonymous block keyword outside mixin body
    - VARIABLE_OUTSIDE_MIXIN: #{var} interpolation outside mixin body
    - MULTIPLE_ATTRIBUTES: two (attrs) blocks on the same tag
    - DUPLICATE_ID: two id shorthands on the same tag (#a#b)

--- packages/parser/test/index.test.js
+++ packages/parser/test/index.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
+var assert = require('node:assert/strict');
 var fs = require('fs');
-var {test} = require('node:test');
+var {test, describe} = require('node:test');
 var parse = require('../');
 var lex = require('pugneum-lexer');
 
@@ -20,5 +21,40 @@ testCases.forEach(function(filename) {
         ast = parse(tokens, {filename: filename});
 
     t.assert.snapshot(ast);
+  });
+});
+
+describe('error paths', () => {
+  function parseSource(src) {
+    var tokens = lex(src, {filename: 'test.pg'});
+    return parse(tokens, {filename: 'test.pg'});
+  }
+
+  test('BLOCK_OUTSIDE_MIXIN when block keyword used outside mixin', () => {
+    assert.throws(
+      () => parseSource('p hello\nblock'),
+      (err) => err.code === 'PUGNEUM:BLOCK_OUTSIDE_MIXIN'
+    );
+  });
+
+  test('VARIABLE_OUTSIDE_MIXIN when #{var} used in text outside mixin', () => {
+    assert.throws(
+      () => parseSource('p #{name}'),
+      (err) => err.code === 'PUGNEUM:VARIABLE_OUTSIDE_MIXIN'
+    );
+  });
+
+  test('MULTIPLE_ATTRIBUTES when tag has two attribute blocks', () => {
+    assert.throws(
+      () => parseSource('div(a="1")(b="2")'),
+      (err) => err.code === 'PUGNEUM:MULTIPLE_ATTRIBUTES'
+    );
+  });
+
+  test('DUPLICATE_ID when tag has two id shorthands', () => {
+    assert.throws(
+      () => parseSource('#a#b'),
+      (err) => err.code === 'PUGNEUM:DUPLICATE_ID'
+    );
   });
 });
